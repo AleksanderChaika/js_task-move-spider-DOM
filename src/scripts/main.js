9e29057ffaf11aba03f8d5d6e7abfdc0532ee865
Aleksander Chaika <alekschayka97@gmail.com>
2025-10-06 14:53:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const spider = document.querySelector('.spider');
-const wall = document.querySelector('.wall');
-
 document.addEventListener('click', (e) => {
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (!wall || !spider) {
+    return;
+  }
+
   if (!e.target.closest('.wall')) {
     return;
   }
@@ -12,8 +16,12 @@ document.addEventListener('click', (e) => {
   const spiderWidth = spider.offsetWidth;
   const spiderHeight = spider.offsetHeight;
 
-  let newLeft = Math.round(e.clientX - wallRect.left - spiderWidth / 2);
-  let newTop = Math.round(e.clientY - wallRect.top - spiderHeight / 2);
+  let newLeft = Math.round(
+    e.clientX - wallRect.left - wall.clientLeft - spiderWidth / 2,
+  );
+  let newTop = Math.round(
+    e.clientY - wallRect.top - wall.clientTop - spiderHeight / 2,
+  );
 
   newLeft = Math.max(0, Math.min(newLeft, wall.clientWidth - spiderWidth));
   newTop = Math.max(0, Math.min(newTop, wall.clientHeight - spiderHeight));
